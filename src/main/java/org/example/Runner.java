29d6d4dc31a7da87aab6466ad44c5afc58d71936
tayfunkilinc <kilinctyfn@gmail.com>
2024-10-30 11:53:11 +0300

    v3

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -3,5 +3,7 @@ package org.example;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello World !");
+
+        System.out.println("version 3");
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,7 +1,5 @@
 package org.example;
 
 public class Student {
- public static void er(){
 
- }
 }
